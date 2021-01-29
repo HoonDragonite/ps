1e97ced79172ce52e3e55537c5cd0a0fda3f59c7
HoonDragonite <lsoonnoon@gmail.com>
2021-01-29 15:16:07 +0900

    baekjoon ps

---
+++ Baekjoon/단계별-문자열/2675번 문자열 반복.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Main {
+	
+	
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		String[] words;
+		int wordCount = 0;
+		int repeat[];
+		
+		wordCount = in.nextInt();
+		words = new String[wordCount];
+		repeat = new int[wordCount];
+		
+		for(int i=0; i<wordCount; i++) {
+			repeat[i] = in.nextInt();
+			words[i] = in.next();
+		}
+		
+		for(int i=0; i<wordCount; i++) {
+			for(int j=0; j<words[i].length(); j++) {
+				for(int k=0; k<repeat[i]; k++) {
+					System.out.print(words[i].charAt(j));
+				}
+			}	
+			System.out.println();
+		}
+	}
+}
