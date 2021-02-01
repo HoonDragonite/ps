bcfecb4877d6938615eec2158a27cc6734f45788
HoonDragonite <lsoonnoon@gmail.com>
2021-02-01 16:45:46 +0900

    baekjoon ps

---
+++ Baekjoon/단계별-문자열/1157번 문자열.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class Main {
+	
+	
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		String word = "";
+		int max = 0;
+		char result = ' ';
+		int[] wordCount = new int[26];
+		
+		word = in.next();
+				
+		word = word.toUpperCase();
+		
+		for(int i=0; i<wordCount.length; i++) {
+			wordCount[i] = 0;
+		}
+		
+		for(int i=0; i<word.length(); i++) {
+			wordCount[word.charAt(i) - 65]++;
+		}
+		
+		for(int i=0; i<wordCount.length; i++) {
+			if(max == wordCount[i]) {
+				max = wordCount[i];
+				result = '?';
+			}
+			else if(max < wordCount[i]) {
+				max = wordCount[i];
+				result = (char) (i+65);
+			}
+		}
+		
+		System.out.println(result);
+	}
+}
