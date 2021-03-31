b19e204f83e5e5b8a1ddd7ee2a3f1714eca12f08
HoonDragonite <lsoonnoon@gmail.com>
2021-03-31 10:18:15 +0900

    PS

---
+++ Programmers/Solution.java
@@ -0,0 +1,15 @@
+package Programmers;
+
+import java.util.*;
+
+public class Solution {
+	public static String solution() {
+        String answer = "";
+        
+        return answer;
+    }
+
+	public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+	}
+}
