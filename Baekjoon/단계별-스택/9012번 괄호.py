5f127276e7c8cda81cbc505011ab9644c07d484a
HoonDragonite <lsoonnoon@gmail.com>
2021-04-21 16:06:40 +0900

    PS

--- Baekjoon/단계별-스택/9012번 괄호.py
+++ Baekjoon/단계별-스택/9012번 괄호.py
@@ -1,3 +1,37 @@
+# 리팩토링 ver1
+# 에러 값을 스택에 저장 안하고 플래그로 처리함
+
+dataArr = []
+dataCount = int(input())
+
+for _ in range(dataCount):
+    dataArr.append(input())
+
+for data in dataArr:
+    stack = []
+    isError = False
+
+    for d in data:
+        if d == "(":
+            stack.append(d)
+        elif d == ")":
+            if len(stack) == 0:
+                isError = True
+                break
+            if stack[-1] == "(":
+                stack.pop()
+            else:
+                isError = True
+    if len(stack) != 0:
+        isError = True
+
+    if isError == True:
+        print("NO")
+    else:
+        print("YES")
+
+'''
+# 원래 풀이
 dataArr = []
 dataCount = int(input())
 
@@ -18,3 +52,5 @@ for data in dataArr:
     if len(stack) == 0:
         print("YES")
     else: print("NO")
+
+'''
