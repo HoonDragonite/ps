9f394a6294599eac0017fb26e76fd855a66f1ab5
HoonDragonite <lsoonnoon@gmail.com>
2021-04-28 09:49:06 +0900

    PS

---
+++ Baekjoon/단계별-브루트포스/1436번 영화감독 숌.py
@@ -0,0 +1,13 @@
+def finalNum(n):
+    count = 0
+    num = 666
+    while True:
+        if "666" in str(num):
+            count += 1
+            if count == n:
+                return num
+        num +=1
+
+n = int(input())
+
+print(finalNum(n))
