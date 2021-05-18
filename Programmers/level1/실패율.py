1b214ce488a6e8c815955086861b04e080b22a8d
HoonDragonite <lsoonnoon@gmail.com>
2021-05-18 16:37:52 +0900

    PS

--- Programmers/level1/실패율.py
+++ Programmers/level1/실패율.py
@@ -1,7 +1,18 @@
+def solution(N, stages):
+    result = {}
+    denominator = len(stages)
+    for stage in range(1, N+1):
+        if denominator != 0:
+            count = stages.count(stage)
+            result[stage] = count / denominator
+            denominator -= count
+        else:
+            result[stage] = 0
+    return sorted(result, key=lambda x : result[x], reverse=True)
+
 '''
-스테이지에 도달했으나 아직 클리어하지 못한 플레이어의 수 / 
-스테이지에 도달한 플레이어 수
-'''
+
+내가 한 풀이(시간 초과)
 
 def solution(N, stages):
     playerCount = len(stages)
@@ -34,7 +45,7 @@ def solution(N, stages):
         answer.append(k)
     
     return answer
-
+'''
 
 N = 5
 stages = [2, 1, 2, 6, 2, 4, 3, 3]	
