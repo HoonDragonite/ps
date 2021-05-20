8f7b0b451f86c3a67780631145a4a7247114af3c
HoonDragonite <lsoonnoon@gmail.com>
2021-05-20 15:35:59 +0900

    PS

---
+++ Programmers/level1/핸드폰 번호 가리기.py
@@ -0,0 +1,6 @@
+def solution(phone_number):
+    answer = "*" * len(phone_number[:-4]) + phone_number[-4:]
+    return answer
+
+print(solution(phone_number="01033334444"))
+print(solution(phone_number="027778888"))
