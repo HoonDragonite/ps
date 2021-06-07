6bb166deda61fc90fa6dc9c7595ecd42729946f5
HoonDragonite <lsoonnoon@gmail.com>
2021-06-07 23:52:36 +0900

    PS

---
+++ Baekjoon/단계별-DFS와 BFS/1260번 DFS와 BFS_LSH.py
@@ -0,0 +1,53 @@
+from collections import deque
+
+def dfs_recursive(x, visited=[]):
+    visited.append(x)
+    for n in graph[x]:
+        if n not in visited:
+            dfs_recursive(n, visited)
+    return visited
+
+def bfs_queue(x, visited=[]):
+    q = deque([x])
+    while q:
+        n = q.popleft()
+        if n not in visited:
+            visited.append(n)
+            for y in graph[n]:
+                if y not in visited:
+                    q.append(y)
+    return visited
+
+# 자료 입력
+n, m, start = map(int, input().split())
+graph = [[] for _ in range(n + 1)]
+
+# 그래프 초기화 
+for _ in range(m):
+    x, y = map(int, input().split())
+    graph[x].append(y)
+    graph[y].append(x)
+for e in graph:
+    e.sort()
+
+# 그래프 출력
+print(graph)
+
+# 탐색 시작
+print(dfs_recursive(start))
+print(bfs_queue(start))
+
+'''
+[Input Example 1]
+4 5 1
+1 2
+1 3
+1 4
+2 4
+3 4
+[Output Example 1]
+1 2 4 3 
+1 2 3 4
+'''
+
+# https://jeinalog.tistory.com/18
--- test.py
+++ test.py
@@ -1,25 +1,3 @@
-from collections import deque
+a = [[], [2, 3, 4], [1, 4], [1, 4], [1, 2, 3]]
 
-n, m, start = map(int, input().split())
-graph = [[] for _ in range(n + 1)]
-
-for _ in range(m):
-    x, y = map(int, input().split())
-    graph[x].append(y)
-    graph[y].append(x)
-
-for e in graph:
-    e.sort()
-
-# 그래프 출력
-print("\n***** Graph *****")
-print(graph)
-print("***** Graph *****\n")
-
-x = 1
-q = deque([x])
-visited = [False] * (n + 1)
-visited[x] = True
-print(q)
-
-
+print(a[1])
