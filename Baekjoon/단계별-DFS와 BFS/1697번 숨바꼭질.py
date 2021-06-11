80ed38f3f6759ddb9ba79c854eb5c10dba377de9
HoonDragonite <lsoonnoon@gmail.com>
2021-06-11 17:06:53 +0900

    PS

--- Baekjoon/단계별-DFS와 BFS/1697번 숨바꼭질.py
+++ Baekjoon/단계별-DFS와 BFS/1697번 숨바꼭질.py
@@ -5,12 +5,14 @@ def bfs_queue(n, k, visited=[]):
     while q:
         n = q.popleft()
         if n not in visited:
-            visited.append(n)
+            visited.append(n) # 현재 n 
+            print("n : " + str(n))
             for y in [n+1, n-1, n*2]:
-                if y != k:
-                    q.append(y)
-                if y == k:
+                if k == y:
                     return visited
+                else:
+                    if y not in visited:
+                        q.append(y)
     return visited
 
 n, k = map(int, input().split()) # 5 17 -> 4
