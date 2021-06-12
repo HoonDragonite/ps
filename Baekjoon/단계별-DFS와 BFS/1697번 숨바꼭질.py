9353f37f422afdc86967627c57fb6a9b9a0fda17
HoonDragonite <lsoonnoon@gmail.com>
2021-06-12 18:55:33 +0900

    PS

--- Baekjoon/단계별-DFS와 BFS/1697번 숨바꼭질.py
+++ Baekjoon/단계별-DFS와 BFS/1697번 숨바꼭질.py
@@ -1,21 +1,53 @@
 from collections import deque
 
+def bfs():
+    q = deque()             # deque는 양쪽에서 입출력 가능
+    q.append(n)             # q = deque([5])
+    while  q:
+        x = q.popleft()     # 처음 시작점은 x = 5, q = deque([])
+        if x == k:
+            print(dist[x])
+            break
+        for nx in (x - 1, x + 1, x * 2):    # nx = 4, 6, 10
+            if 0 <= nx <= MAX and not dist[nx]:
+                dist[nx] = dist[x] + 1
+                q.append(nx)    # q = deque([4, 6, "10"])
+
+MAX = 10 ** 5               # 시간초과 안나게 수 제한
+dist = [0] * (MAX + 1)      # 이동하는 거리를 알기 위한 변수
+n, k = map(int, input().split())
+
+bfs()
+
+'''
+시간 초과
+
+from collections import deque
+
 def bfs_queue(n, k, visited=[]):
     q = deque([n])
+    addQ = deque([])
+   
+    if n == k:
+        return 0
+
+    depth = 1
     while q:
         n = q.popleft()
         if n not in visited:
             visited.append(n) # 현재 n 
-            print("n : " + str(n))
             for y in [n+1, n-1, n*2]:
                 if k == y:
-                    return visited
+                    return depth
                 else:
                     if y not in visited:
-                        q.append(y)
-    return visited
+                        addQ.append(y)
+        if len(q) == 0:
+            q.extend(addQ)
+            addQ.clear()
+            depth +=1
 
 n, k = map(int, input().split()) # 5 17 -> 4
 
 print(bfs_queue(n, k))
-print(len(bfs_queue(n, k)))
+'''
